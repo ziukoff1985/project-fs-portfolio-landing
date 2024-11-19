0054108f70d3616b7008e2b96ba5f640773e4e55
Pavlo-Chernicenko <pwned7067@gmail.com>
2024-11-19 19:16:57 +0200

    Initialization

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/projects';
+import './js/faq';
+import './js/reviews';
+import './js/work-together';
+
+import './js/mob-menu';
+import './js/modal-window';
+
+import './js/mod';
---
+++ vite.config.js
@@ -0,0 +1,76 @@
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
+import imagemin from 'vite-plugin-imagemin';
+
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
+        },
+      },
+      outDir: '../dist',
+      emptyOutDir: true,
+    },
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+      imagemin({
+        gifsicle: {
+          optimizationLevel: 7,
+        },
+        optipng: {
+          enabled: true,
+        },
+        mozjpeg: {
+          quality: 80,
+        },
+        pngquant: {
+          quality: [0.65, 0.9],
+          speed: 4,
+        },
+        svgo: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  removeViewBox: false,
+                },
+              },
+            },
+          ],
+        },
+      }),
+    ],
+  };
+});
