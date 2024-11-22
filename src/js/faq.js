26f4a9e99a69f22a981a6c3e6fd0d5829168511c
Khrystyna Storozhuk <khrystynastorozhuk72@gmail.com>
2024-11-21 22:09:09 -0500

    finished changes

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,22 +1,16 @@
-// 'use strict';
-// import Accordion from 'accordion-js';
-// import 'accordion-js/dist/accordion.min.css';
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const accordion = new Accordion('.accordion', {
-//     duration: 300,
-//     showMultiple: false,
-//   });
-//   console.log(accordion);
-// });
-document.querySelectorAll('.wrapper-faq').forEach(item => {
-  item.addEventListener('click', function () {
-    const parent = this.parentElement;
-    document.querySelectorAll('.accordion-item').forEach(accItem => {
-      if (accItem !== parent) {
-        accItem.classList.remove('active');
-      }
-    });
-    parent.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', () => {
+  const accordion = new Accordion('#ac-list', {
+    closeOther: true,
   });
+
+  const firstAccordionItem = document.querySelector('#ac-list .accordion-item');
+  const firstAccordionTrigger =
+    firstAccordionItem?.querySelector('.ac-trigger');
+
+  if (firstAccordionTrigger) {
+    firstAccordionTrigger.click();
+  }
 });
