7bafd92a7d6aac728f715e35c20dc41855bf7e5c
Pavlo-Chernicenko <pwned7067@gmail.com>
2024-11-22 10:36:10 +0200

    Optimized opening of the mobile window

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,49 +1,18 @@
-(() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+const mobileMenu = document.querySelector('.mob-menu');
+const openMenuBtn = document.querySelector('#header .js-open-menu');
+const arrCloseBtn = document.querySelectorAll('.mob-menu [data-close="true"]');
 
-  const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
+function openMenu() {
+  mobileMenu.classList.add('is-open');
+  document.body.style.overflow = 'hidden';
+}
 
-    if (!isMenuOpen) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = '';
-    }
-  };
+function closeMenu() {
+  document.body.style.overflow = '';
+  mobileMenu.classList.remove('is-open');
+}
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-
-  const anchors = mobileMenu.querySelectorAll('a[href*="#"]');
-  anchors.forEach(anchor => {
-    anchor.addEventListener('click', e => {
-      e.preventDefault();
-
-      const targetId = anchor.getAttribute('href').slice(1);
-      const targetElement = document.getElementById(targetId);
-
-      if (targetElement) {
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-      }
-
-      toggleMenu();
-      window.history.pushState(null, null, window.location.pathname);
-    });
-  });
-
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    document.body.style.overflow = '';
-  });
-
-  window.addEventListener('load', () => {
-    window.scrollTo(0, 0);
-  });
-})();
+openMenuBtn.addEventListener('click', openMenu);
+arrCloseBtn.forEach(e => {
+  e.addEventListener('click', closeMenu);
+});
