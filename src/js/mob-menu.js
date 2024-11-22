271cf7984d75eb27e3158b1c17064b2ddaca322b
Liza <lizaapostol378@gmail.com>
2024-11-22 17:26:02 +0200

    add animation

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -3,12 +3,18 @@ const openMenuBtn = document.querySelector('#header .js-open-menu');
 const arrCloseBtn = document.querySelectorAll('.mob-menu [data-close="true"]');
 
 function openMenu() {
-  mobileMenu.classList.add('is-open');
-  document.body.style.overflow = 'hidden';
+  openMenuBtn.classList.toggle('is-active');
+  mobileMenu.classList.toggle('is-open');
+  if (mobileMenu.classList.contains('is-open')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = '';
+  }
 }
 
 function closeMenu() {
   document.body.style.overflow = '';
+  openMenuBtn.classList.remove('is-active');
   mobileMenu.classList.remove('is-open');
 }
 
