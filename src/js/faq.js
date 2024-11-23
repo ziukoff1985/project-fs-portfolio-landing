fb9a6709176473509818271bf1d0765fbfec627f
Pavlo-Chernicenko <pwned7067@gmail.com>
2024-11-23 10:31:46 +0200

    Fixed js

--- src/js/faq.js
+++ src/js/faq.js
@@ -4,14 +4,6 @@ import 'accordion-js/dist/accordion.min.css';
 document.addEventListener('DOMContentLoaded', () => {
   const accordion = new Accordion('#ac-list', {
     closeOther: true,
-    openFirst: true,
   });
-
-  const firstAccordionItem = document.querySelector('#ac-list .accordion-item');
-  const firstAccordionTrigger =
-    firstAccordionItem?.querySelector('.ac-trigger');
-
-  if (firstAccordionTrigger) {
-    firstAccordionTrigger.click();
-  }
+  accordion.open(0);
 });
--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -2,6 +2,8 @@ const mobileMenu = document.querySelector('.mob-menu');
 const openMenuBtn = document.querySelector('#header .js-open-menu');
 const arrCloseBtn = document.querySelectorAll('.mob-menu [data-close="true"]');
 
+openMenuBtn.addEventListener('click', openMenu);
+
 function openMenu() {
   openMenuBtn.classList.toggle('is-active');
   mobileMenu.classList.toggle('is-open');
@@ -10,15 +12,19 @@ function openMenu() {
   } else {
     document.body.style.overflow = '';
   }
+
+  arrCloseBtn.forEach(e => {
+    e.addEventListener('click', closeMenu);
+  });
 }
 
 function closeMenu() {
   document.body.style.overflow = '';
   openMenuBtn.classList.remove('is-active');
   mobileMenu.classList.remove('is-open');
-}
+  openMenuBtn.addEventListener('click', openMenu);
 
-openMenuBtn.addEventListener('click', openMenu);
-arrCloseBtn.forEach(e => {
-  e.addEventListener('click', closeMenu);
-});
+  arrCloseBtn.forEach(e => {
+    e.removeEventListener('click', closeMenu);
+  });
+}
