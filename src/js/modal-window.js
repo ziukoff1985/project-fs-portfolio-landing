4486b293e0514d2ebada64c184cf5319cd330500
Pavlo-Chernicenko <pwned7067@gmail.com>
2024-11-24 12:01:46 +0200

    Fixed modal window

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -3,23 +3,27 @@ const closeArr = document.querySelectorAll('[data-close-modal]');
 
 export function modalWindowOpen() {
   modalWindow.classList.add('is-open');
+  document.addEventListener('keydown', modalWindowClose);
 
   closeArr.forEach(elm => {
     elm.addEventListener('click', modalWindowClose);
-    document.addEventListener('keydown', closeEsc);
   });
 }
 
-function modalWindowClose() {
-  modalWindow.classList.remove('is-open');
-  document.body.style.overflow = '';
+function modalWindowClose(event) {
+  closeArr.forEach(closeElm => {
+    if (
+      event.target === closeElm ||
+      event.target.closest('.btn-close') === closeElm ||
+      event.key === 'Escape'
+    ) {
+      modalWindow.classList.remove('is-open');
+      document.body.style.overflow = '';
+      document.removeEventListener('keydown', closeEsc);
 
-  closeArr.forEach(elm => {
-    elm.removeEventListener('click', modalWindowClose);
-    document.removeEventListener('keydown', closeEsc);
+      closeArr.forEach(elm => {
+        elm.removeEventListener('click', modalWindowClose);
+      });
+    }
   });
-}
-
-function closeEsc(e) {
-  if (e.key === 'Escape') modalWindowClose();
 }
