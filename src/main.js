0f7bcb94eafd2134f91c2402728f3a192935265f
Borys Ziukov <ziukoff1985@gmail.com>
2024-11-30 19:47:46 +0200

    started adding comments

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
+// Імпорти модулів для різних функціональних блоків сайту
 import './js/observer';
 import './js/render-projects';
 import './js/projects';
 import './js/faq';
 import './js/reviews';
 import './js/work-together';
-
 import './js/mob-menu';
 import './js/modal-window';
-
 import './js/changes-mod';
 import './js/scroll-to-top';
